b28af835349747d1cf9c4fa813033d71f1f35ac7
Wylham Lima <enaironwyllam10@gmail.com>
2025-07-20 14:21:31 -0300

    Adicionar arquivos de projeto.

---
+++ VisualizadorDeProdutos/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace VisualizadorDeProdutos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "CHEVROLET", "FIAT", "HYUNDAI", "RENAULT", "VOLKSWAGEN" });
+            comboBox1.Location = new Point(159, 231);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(196, 28);
+            comboBox1.TabIndex = 0;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(159, 324);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(196, 28);
+            comboBox2.TabIndex = 1;
+            comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Blauer Nue", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(60, 231);
+            label1.Name = "label1";
+            label1.Size = new Size(72, 24);
+            label1.TabIndex = 2;
+            label1.Text = "Marca";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Blauer Nue", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(60, 328);
+            label2.Name = "label2";
+            label2.Size = new Size(82, 24);
+            label2.TabIndex = 3;
+            label2.Text = "Modelo";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(60, 78);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(295, 76);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.Location = new Point(401, 78);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(565, 420);
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            BackgroundImageLayout = ImageLayout.None;
+            ClientSize = new Size(1019, 561);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Name = "Form1";
+            Text = "Visualizar Modelos";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Label label1;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+    }
+}
---
+++ VisualizadorDeProdutos/Form1.cs
@@ -0,0 +1,77 @@
+namespace VisualizadorDeProdutos {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e) {
+            string[] arquivos = Directory.GetFiles(@"C:\C#\Projeto Visualizador de Produtos\Carros"); // busca as imagens direto deste local
+
+            foreach (string caminhoCompleto in arquivos) {
+                string nomeArquivoSemExtensao = Path.GetFileNameWithoutExtension(caminhoCompleto);
+
+                if (comboBox2.Text == nomeArquivoSemExtensao) {
+                    if (File.Exists(caminhoCompleto)) {
+                        pictureBox2.ImageLocation = caminhoCompleto;
+                        pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                    }
+                    else {
+                        MessageBox.Show("Imagem não encontrada em: " + caminhoCompleto);
+                    }
+
+                    return; // para evitar iteração desnecessária
+                }
+            }
+
+            MessageBox.Show("Imagem não encontrada para o modelo selecionado.");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) {
+
+            comboBox2.Text = string.Empty;
+
+            if (comboBox1.Text == "FIAT") {
+
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("ARGO DRIVE 1.0");
+                comboBox2.Items.Add("CRONOS DRIVE 1.0");
+                comboBox2.Items.Add("MOBI LIKE 1.0");
+            }
+
+            else if (comboBox1.Text == "HYUNDAI") {
+
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("HB20 COMFORT PLUS 1.0");
+                comboBox2.Items.Add("HB20S COMFORT PLUS 1.0");
+
+            }
+
+            else if (comboBox1.Text == "CHEVROLET") {
+
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("ONIX PLUS LTZ 1.0");
+                comboBox2.Items.Add("ONIX LT 1.0");
+                comboBox2.Items.Add("TRACKER LT 1.0");
+
+            }
+
+            else if (comboBox1.Text == "VOLKSWAGEN") {
+
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("POLO TRACK 1.0");
+                comboBox2.Items.Add("T-CROSS 1.0");
+                comboBox2.Items.Add("NIVUS HIGHLINE 1.0");
+
+            }
+
+            else if (comboBox1.Text == "RENAULT") {
+
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("KWID ZEN 1.0");
+                comboBox2.Items.Add("DUSTER ICONIC 1.6");
+                comboBox2.Items.Add("OROCH PRO 1.6");
+
+            }
+        }
+    }
+}
